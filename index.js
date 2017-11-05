5c962399d1b2d06676863face641807b9ce8205b
Ian Pavlovic <ian_pavlovic@msn.com>
2017-11-05 20:39:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(katzDeliLine, name){
 katzDeliLine.push(name)
 return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length !== 0){
+    return katzDeliLine[0]
+  }else{
+    return "There is nobody waiting to be served!"
+  }
+return katzDeliLine.unshift()
+}
